a54e7b0d684cea74c81b006dcbabae33ef01e5b1
Jesper Tetsche Wittendorff Eriksen (JYG) <JYG@kmd.dk>
2022-04-26 10:28:00 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calendar_Demo';
+
+  calendarOptions: CalendarOptions = {
+    initialView: 'listWeek',
+    events:  [
+      {
+        title  : 'Møde 1',
+        start  : '2022-04-25T12:00:00',
+        end    : '2022-04-25T12:30:00', 
+        description: 'first description' 
+      },
+      {
+        title  : 'Møde 2',
+        start  : '2022-04-26T13:00:00',
+        end    : '2022-04-26T13:30:00', 
+        description: 'first description' 
+      },
+      {
+        title  : 'Møde 3',
+        start  : '2022-04-27T11:00:00',
+        end    : '2022-04-27T11:30:00', 
+        description: 'first description' 
+      },
+      {
+        title  : 'Ingen møder denne dag',
+        start  : '2022-04-28',
+        end    : '2022-04-28',
+        allDay : true,
+      },
+      {
+        title  : 'Møde 5',
+        start  : '2022-04-29T15:00:00',
+        end    : '2022-04-29T15:30:00', 
+        description: 'first description' 
+      }
+    ],
+    showNonCurrentDates: true,
+    views: {
+      listDay: { buttonText: 'list day' },
+      listWeek: { buttonText: 'list week' },
+      listMonth: { buttonText: 'list month' }
+    },
+    headerToolbar: {
+      left: 'title',
+      center: '',
+      right: 'listDay,listWeek,listMonth'
+    },
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FullCalendarModule } from '@fullcalendar/angular'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import  ListView  from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin,
+  ListView
+]);
 
 @NgModule({
   declarations: [
@@ -10,7 +19,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule // register FullCalendar with you app
+
   ],
   providers: [],
   bootstrap: [AppComponent]
